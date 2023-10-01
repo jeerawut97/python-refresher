aaa8704f85d003f027065fda91697b823b02cf3d
JRW <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1@github.com>
2023-10-01 15:11:36 +0700

    type hinting

---
+++ 29_imports_in_python/code.py
@@ -0,0 +1,7 @@
+from mymodule import divide
+
+print(divide(10, 2))
+print(__name__)
+
+import sys
+print(sys.modules)
---
+++ 30_errors_in_python/code.py
@@ -0,0 +1,18 @@
+def divide(dividend, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0.")
+    
+    return dividend / divisor
+
+grades = []
+print("Welcome to the average grade program.")
+try:
+    average = divide(sum(grades), len(grades))
+except ZeroDivisionError:
+    print("There are no grades yet in your list.")
+except ValueError as e:
+    print(e)
+else:
+    print(f"The average grade is {average}.")
+finally:
+    print('Thank you!')
